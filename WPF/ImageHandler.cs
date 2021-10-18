e446dd3a3d5300056c3a8bbe115e3488978b4512
Berne Gerrits <31410921+Faalangst26@users.noreply.github.com>
2021-10-18 15:07:29 +0200

    Caching

---
+++ WPF/ImageHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace WPF
+{
+    public static class ImageHandler
+    {
+        private static Dictionary<string, Bitmap> _ImageCache = new Dictionary<string, Bitmap>();
+
+        public static Bitmap GetImage(string imgurl)
+        {
+            if (_ImageCache.ContainsKey(imgurl))
+            {
+                return _ImageCache[imgurl];
+            }
+            else
+            {
+                Bitmap bm = new Bitmap(imgurl);
+                _ImageCache[imgurl] = bm;
+                return bm;
+            }
+        }
+
+        public static void ClearCache()
+        {
+            _ImageCache.Clear();
+        }
+
+        public static Bitmap GetBackground(int len, int width)
+        {
+            if (_ImageCache.ContainsKey("empty"))
+            {
+                return _ImageCache["empty"];
+            }
+            else //Er is nog geen empty Bitmap in de cache, maak een nieuwe aan.
+            {
+                Bitmap background = new Bitmap(len, width);
+                var graphics = Graphics.FromImage(background);
+                graphics.FillRectangle(Brush, 0, 0, len, width);
+            }
+        }
+    }
+}
