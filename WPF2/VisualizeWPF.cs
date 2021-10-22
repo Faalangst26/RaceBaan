4eb9ee193477b0e28e01b92d5e1d5f0b4fe32799
Berne Gerrits <31410921+Faalangst26@users.noreply.github.com>
2021-10-22 14:01:42 +0200

    Geen teringzooi meer, nog niet perferct

--- WPF2/VisualizeWPF.cs
+++ WPF2/VisualizeWPF.cs
@@ -22,10 +22,10 @@ namespace WPF2
         #endregion
 
         private static int _direction = 1;
-        private static Point cursorpos = new Point(0, 0);
+        private static Point cursorpos = new Point(100, 200);
         public static BitmapSource DrawTrack(Track track)
         {
-            Bitmap trackimg = new Bitmap(ImageHandler.GetBackground(200, 200));
+            Bitmap trackimg = new Bitmap(ImageHandler.GetBackground(400, 400));
             Graphics gr = Graphics.FromImage(trackimg);
             //1 is richting het oosten, 2 richting het zuiden, 3 het westen, en 0 het noorden 
             foreach (Section section in track.Sections)
@@ -55,18 +55,23 @@ namespace WPF2
                 //Bochten naar rechts
                 if (section.SectionType.Equals(SectionTypes.RightCorner)){
                     _direction++;
-                    gr.DrawImage(Image.FromFile(corner_right), cursorpos);
-               }
+                    var img = Image.FromFile(corner_left);
+                    img.RotateFlip(Calcrotation());
+                    gr.DrawImage(img, cursorpos);
+                }
                 //Bochten naar links
                 if (section.SectionType.Equals(SectionTypes.LeftCorner)){
                     _direction--;
-                    gr.DrawImage(Image.FromFile(corner_left), cursorpos);
+                    var img = Image.FromFile(corner_left);
+                    img.RotateFlip(Calcrotation());
+                    gr.DrawImage(img, cursorpos);
                 }
 
 
                 //Cursor en draaing bepalen
                 if (_direction == 0 || _direction > 3)//Noorden
                 {
+                    
                     _direction = 0;
                     cursorpos.Y += 50;
                 }
@@ -90,6 +95,30 @@ namespace WPF2
             var Bitmapsource = ImageHandler.CreateBitmapSourceFromGdiBitmap(trackimg);
 
             return Bitmapsource;
+        }
+
+        public static RotateFlipType Calcrotation()
+        {
+            if(_direction == 3 || _direction < 0)
+            {
+                return RotateFlipType.RotateNoneFlipNone;
+            }
+            else if(_direction == 2)
+            {
+                return RotateFlipType.Rotate90FlipNone;
+            }
+            else if(_direction == 1)
+            {
+                return RotateFlipType.Rotate180FlipNone;
+            } 
+            else if(_direction == 0 || _direction > 3)
+            {
+                return RotateFlipType.Rotate270FlipNone;
+            } else
+            {
+                return RotateFlipType.RotateNoneFlipNone;
+            }
+
         }
     }
 }
